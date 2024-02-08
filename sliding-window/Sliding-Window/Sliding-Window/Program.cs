348b38acb2038b3d01050c1fa1cfa6675df64272
Caio Camargo <caiocbahia@hotmail.com>
2024-02-07 22:06:13 -0300

    Two Sum - Bruteforce Solution

---
+++ sliding-window/Sliding-Window/Sliding-Window/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int LongestSubarray(int[] nums)
+{
+	int Size = nums.Length;
+
+	int Lo = 0;
+	int Hi = 0;
+
+	int Curr = 0;
+	int Max = 0;
+
+	int Zeroes = 0;
+
+	// Enquanto o hi for menor do que o size, vamos expandir
+	// Quando o Zeroes atingir >1, a janela vai contrair (o lo vai aumentar)
+
+
+	return 10;
+}
---
+++ two-sum/Two-Sum/brute-force-solution/Two-Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+int[] TwoSum(int[] nums, int target)
+{
+	for (int i = 0; i < nums.Length; i++)
+	{
+		for (int j = i + 1; j < nums.Length; j++)
+		{
+			if (nums[i] + nums[j] == target)
+			{
+				return new int[] { i, j };
+			}
+		}
+	}
+	throw new Exception("Not found");
+}
+Console.WriteLine("Os índices do array que somados resultam no target é: " + string.Join(", ", TwoSum([1, 2, 7, 10, 23], 9)));
+
+//Console.WriteLine("Resultado: " + TwoSum([1, 2, 7, 10, 23], 9));

a5aeecf17c6035be13a3dfeb675643aee1df04d8
Caio Camargo <caiocbahia@hotmail.com>
2024-02-13 23:09:34 -0300

    Find first palindrome - first solution(using reverse)

---
+++ FindFirstPalindrome/FindFirstPalindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿static string FirstPalindrome(string[] words)
+{
+	foreach (string w in words)
+	{
+		if (w == String.Join("", w.Reverse()))
+		{
+			return w;
+		}
+	}
+
+	return "";
+}
+Console.WriteLine(FirstPalindrome(["abc", "car", "racecar", "ada", "cool"]));
+
+
+
+// Maneira alternativa de reverter um array:
+
+// Primeiro, TRANSFORMAMOS A STRING EM UM ARRAY DE CHAR!!
+// Um char é uma string de apenas um caractere. Quando temos um array de chars, temos algo equivalente a uma string
+// O método ToCharArray() dará conta de fazer essa tarefa
+
+// Para reverter o array, usamos o método Reverse
+
+//static string Reverted(string s)
+//{
+//	char[] charArray = s.ToCharArray();
+//	Array.Reverse(charArray);
+//	return new string(charArray);
+//}
+
+//Console.WriteLine(Reverted("Carro"));

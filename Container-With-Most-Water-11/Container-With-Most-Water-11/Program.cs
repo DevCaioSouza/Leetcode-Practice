5759b47ca5f71ed3b9e8c1dd61b756fb839d5223
Caio Camargo <caiocbahia@hotmail.com>
2024-02-29 21:33:54 -0300

    Two Pointer Solution

---
+++ Container-With-Most-Water-11/Container-With-Most-Water-11/Program.cs
@@ -0,0 +1,26 @@
+﻿
+static int MaxArea(int[] height)
+{
+
+	int LeftP = 0;
+	int RightP = height.Length - 1;
+	int MaxArea = 0;
+
+	while (LeftP < RightP)
+	{
+		int CurrArea = Math.Min(height[LeftP], height[RightP]) * (RightP - LeftP);
+
+		MaxArea = Math.Max(MaxArea, CurrArea);
+		if (height[RightP] > height[LeftP])
+		{
+			LeftP++;
+		}
+		else
+		{
+			RightP--;
+		}
+	}
+	return MaxArea;
+}
+
+Console.WriteLine(MaxArea([1, 3, 2, 5, 25, 24, 5]));
